3cdd169b29ac16ae2e626d2f471b5ecca5bb38be
Ricky Sandoval <rsandoval@arrow.com>
2016-10-06 21:00:15 -0600

    Start adding css, add header template

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+
+gulp.task('sass', function() {
+	return gulp.src('./sass/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(rename("custom.css"))
+	.pipe(gulp.dest("./css"));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});

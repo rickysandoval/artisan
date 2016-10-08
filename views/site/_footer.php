acfc9405e63f2b079894639612e19d4388a673f1
Ricky Sandoval <rsandoval@arrow.com>
2016-10-07 23:39:00 -0600

    work on footer

---
+++ views/site/_footer.php
@@ -0,0 +1,46 @@
+<div id="footer" class="row-fluid">
+	<div class="full-width footer-top">
+		<div class="container-fluid" id="container">
+			<div class="span12">
+				<div class="addresshours">
+					<div id="cbinfo" class="span4">
+						<div class="footer__owner">Jeff Deutsch</div>
+						<div>311 Elk Ave </div>
+						<div>Crested Butte, CO 81224</div>
+						<div>(970) 349-0016</div>
+						<div>jeff@artisanruggallery.com</div>
+					
+					</div>
+					<div class="footer-middle span4">
+						<?php echo CHtml::link(CHtml::image(Yii::app()->theme->baseUrl.'/css/images/logo-footer.jpg'), Yii::app()->baseUrl."/"); ?>
+					</div>
+					<div id="denverinfo" class="span4">
+						<div class="footer__owner">Kathy Discoe</div>
+						<div>Shops at 9th 899 N Broaday </div>
+						<div>Denver, CO 80203 </div>
+						<div>(303) 825-0064</div>
+						<div>kathy@artisanruggallery.com</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="full-width footer-bottom">
+		<div class="container-fluid" id="container">
+			<div class="row-fluid footer__bottom">
+				<div class="bottomtabs span4">
+					<?php
+						foreach (CustomPage::model()->bottomtabs()->findAll() as $arrTab)
+							echo CHtml::link(Yii::t('global',$arrTab->title),$arrTab->Link).' / ';
+					echo CHtml::link(Yii::t('global','Sitemap'),$this->createUrl('site/map'));
+					?>
+				</div>
+				<div class="copyright span4">
+					&copy; <?= Yii::t('global', 'Copyright') ?> <?= date("Y"); ?> <?= _xls_get_conf('STORE_NAME') ?>. <?= Yii::t('global', 'All Rights Reserved'); ?>.
+				</div>
+				<div class="social span4"></div>
+			</div>
+		</div>
+	</div>
+</div>
+

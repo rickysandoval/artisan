de739a91062c99e807e694a8d26a8e0ce997d898
Ricky Sandoval <rsandoval@arrow.com>
2018-02-03 22:24:12 -0700

    udpates

--- lightspeed-tester/assets/global.js
+++ lightspeed-tester/assets/global.js
@@ -84,7 +84,7 @@ $(document).ready(function(){
   $('#marketingBanner').on('click', openMarketingModal);
   $('.marketing-modal__close').on('click', closeMarketingModal);
   $('.marketing-modal__overlay').on('click', closeMarketingModal);
-  $('#registerPromo').on('click', closeMarketingModal);
+  $('.marketing-modal__action a').on('click', closeMarketingModal);
 
 
   function openMarketingModal() {
